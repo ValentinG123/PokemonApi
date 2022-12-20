5dd8dcb8f1bcb38b1695e60cb534aed1b1542fa5
ValentinG123 <valentingraglia18@gmail.com>
2022-12-20 00:19:46 -0300

    ApiCall and first component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
+import axios from "axios";
+import React,{useState,useEffect} from "react";
+import Main from "./components/Main";
+import Navbar from './components/Navbar'
 function App() {
+  const [name,setName] = useState('')
+useEffect(() => {
+
+    const getPokemon = async () => {
+      const url = `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=1/`
+      const response = await axios.get(url)
+      console.log(response)
+    }
+ getPokemon()
+ 
+}, [])
+
   return (
-    <div className="App">
- Hola
+    <div className="max-w-[420px] bg-black h-screen">
+      <Navbar/>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='flex justify-center flex-col items-center'>
+        <h2 className='text-white text-2xl my-8'>
+        Wich pokemon are you<br/>Loking for
+        </h2>
+        <div className='bg-gray-500 w-72 h-8 rounded-full'>
+
+        </div>
+        </div>
+  )
+}
+
+export default Main
